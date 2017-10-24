a8fd9164329d7a815cfd4b7309fdab89722ff1e4
Renato Dias <lrds1722@gmail.com>
2017-10-24 13:56:29 -0300

    Funcionalidade do jogo para Aprendizagem, completo.

--- Assets/TelaAprendizagem.cs
+++ Assets/TelaAprendizagem.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
-//using System;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 
@@ -32,16 +32,29 @@ public struct JogoAprendiazgem {
 public class TelaAprendizagem : MonoBehaviour {
 
 	public Text pergunta;
+	public string resposta;
+	public int pontosAcerto = 0;
+	public int pontosErros= 0;
 	public Button resposta1;
 	public Button resposta2;
 	public Button resposta3;
 	public Button resposta4;
 
+	public Text pontuacaoAcerto;
+	public Text pontuacaoErro;
+	public GameObject panelGame;
+	public GameObject panelPontuacao;
+
+	public Text time;
+	public int timerSeconds = 29;
 
 	public List<JogoAprendiazgem> jogos = new List<JogoAprendiazgem> ();
 
 	// Use this for initialization
 	void Start () {
+		
+		Invoke("FinalPartida", 29.0f);
+		this.timerSeconds = 29;
 		jogos.Add (new JogoAprendiazgem ("2+3=", "5", "1", "3", "5", "2"));
 		jogos.Add (new JogoAprendiazgem("2-3=", "-1", "-1", "2", "0", "1"));
 		jogos.Add (new JogoAprendiazgem("(2-3)+2=", "1", "-1", "2", "0", "1"));
@@ -49,7 +62,9 @@ public class TelaAprendizagem : MonoBehaviour {
 		jogos.Add (new JogoAprendiazgem ("6-3=", "3", "5", "2", "3", "4"));
 		jogos.Add (new JogoAprendiazgem ("1-9=", "-8", "10", "-8", "-10", "0"));
 		jogos.Add (new JogoAprendiazgem ("-40+50=", "10", "-10", "10", "0", "1"));
-		StartGame ();
+		this.time.text = "" + timerSeconds;
+		UpdatePerguntas();
+		InvokeRepeating ("CountDown", 0f, 1.0f);
 	}
 	
 	// Update is called once per frame
@@ -57,25 +72,56 @@ public class TelaAprendizagem : MonoBehaviour {
 		
 	}
 
-	public void StartGame(){
-		InvokeRepeating("UpdatePerguntas", 1.0f, 5.0f);
-	}
+//	public void StartGame(){
+//		InvokeRepeating("UpdatePerguntas", 5.0f, 5.0f);
+//	}
 
 	public void Resposta_Selecionada_Click(GameObject sender) {
 		if (sender.name == "ButtonResposta1") {
-			
+			if (resposta == resposta1.GetComponentInChildren<Text> ().text) {
+				this.pontosAcerto++;
+			} else {
+				this.pontosErros++;
+			}
+
 		}
 
 		if (sender.name == "ButtonResposta2") {
-			
+			if (resposta == resposta2.GetComponentInChildren<Text> ().text) {
+				this.pontosAcerto++;
+			} else {
+				this.pontosErros++;
+			}
 		}
 
 		if (sender.name == "ButtonResposta3") {
-			
+
+			if (resposta == resposta3.GetComponentInChildren<Text> ().text) {
+				this.pontosAcerto++;
+			} else {
+				this.pontosErros++;
+			}
 		}
 
 		if (sender.name == "ButtonResposta4") {
-			
+			if (resposta == resposta4.GetComponentInChildren<Text> ().text) {
+				this.pontosAcerto++;
+			} else {
+				this.pontosErros++;
+			}
+		}
+		UpdatePerguntas ();
+	}
+
+	public void Pontuacao_Button_Selecionar_Click(GameObject sender) {
+		if (sender.name == "ButtonRepetir") {
+			this.panelPontuacao.SetActive(false);
+			this.panelGame.SetActive (true);
+			Start ();
+		}
+
+		if (sender.name == "ButtonVoltar") {
+			FinalGame ();
 		}
 	}
 
@@ -84,10 +130,30 @@ public class TelaAprendizagem : MonoBehaviour {
 
 		JogoAprendiazgem j = jogos [Random.Range(0, jogos.Count-1)];
 		pergunta.text = j.pergunta;
+		this.resposta = j.resposta;
 		resposta1.GetComponentInChildren<Text>().text = j.pergunta1;
 		resposta2.GetComponentInChildren<Text>().text = j.pergunta2;
 		resposta3.GetComponentInChildren<Text>().text = j.pergunta3;
 		resposta4.GetComponentInChildren<Text>().text = j.pergunta4;
 	}
+
+	public void FinalGame(){
+		SceneManager.LoadScene ("TelaPrincipal", LoadSceneMode.Single);
+	}
+
+	public void FinalPartida(){
+		this.pontuacaoAcerto.text = pontosAcerto.ToString();
+		this.pontuacaoErro.text = pontosErros.ToString();
+		this.panelPontuacao.SetActive(true);
+		this.panelGame.SetActive (false);
+	}
+
+	void CountDown() {
 		
+		this.time.text =timerSeconds.ToString();
+		timerSeconds--;
+		if(timerSeconds < 1) {
+			CancelInvoke("CountDown");
+		}
+	}
 }
