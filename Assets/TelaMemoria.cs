829903c62aec730ac04ac4403bd4faa6a003bb70
Renato Dias <lrds1722@gmail.com>
2017-10-26 15:31:32 -0300

    Add tela memoria

---
+++ Assets/TelaMemoria.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TelaMemoria : MonoBehaviour {
+
+	public int pontosAcerto = 0;
+	public int pontosErros= 0;
+	public Text pontuacaoAcerto;
+	public Text pontuacaoErro;
+	public GameObject panelGame;
+	public GameObject panelPontuacao;
+
+	public Text time;
+	public int timerSeconds = 29;
+
+	// Use this for initialization
+	void Start () {
+
+		this.pontosAcerto = 0;
+		this.pontosErros= 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Pontuacao_Button_Selecionar_Click(GameObject sender) {
+		if (sender.name == "ButtonRepetir") {
+			this.panelPontuacao.SetActive(false);
+			this.panelGame.SetActive (true);
+			Start ();
+		}
+
+		if (sender.name == "ButtonVoltar") {
+//			FinalGame ();
+		}
+	}
+}
